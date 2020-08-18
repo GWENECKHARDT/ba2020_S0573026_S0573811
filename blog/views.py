c1eab4c41d612f1b36b80eaba4ee5a38bfcbe457
Vivien Vanessa Bentzen <s0573026@htw-berlin.de>
2020-08-18 20:41:26 +0200

    Login/Logout Funktion verbessert, sodass man nicht mehr auf die To-Do Listen zugreifen kann, wenn man nicht eingeloggt ist. Neue To-Do hinzufügen Funktion wieder hergestellt.

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render, get_object_or_404
-from django.shortcuts import redirect
+from django.contrib import messages
 from django.utils import timezone
-
-from .forms import PostForm
-
-# Create your views here.
 from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 
+
+@login_required
+def todo_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/todo_list.html', {'posts': posts})
+
+
+@login_required
+def todo_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/todo_detail.html', {'post': post})
+
+
+@login_required
 def todo_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -21,9 +34,8 @@ def todo_new(request):
         form = PostForm()
     return render(request, 'blog/todo_edit.html', {'form': form})
 
-def todo_list(request):
-    return render(request, 'blog/todo_list.html', {})
 
+@login_required
 def todo_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -37,7 +49,3 @@ def todo_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/todo_edit.html', {'form': form})
-
-def todo_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/todo_detail.html', {'todo': post})
